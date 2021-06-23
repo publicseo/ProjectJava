e5af8f98f73787eb9e6c09fdf80d5b5724f77854
goldgym <goldgym@gccompany.co.kr>
2021-06-23 12:39:09 +0900

    Initial commit

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,31 @@
+/**
+ * @authoer goldgym
+ * @return
+ * @since 2021/06/23
+ */
+
+package com.ProjectJava.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String home(Model model){
+    model.addAttribute("msg", "Hello World!");
+    return "index";
+  }
+
+  @GetMapping("/anotherpage")
+  public ModelAndView buttonTest(){
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("index");
+    modelAndView.addObject("msg", "다른 페이지로 이동하였습니다!!!");
+    return modelAndView;
+  }
+
+}
